5e7a5a14ce20fe054bf62857199056567c8cb425
Porter Peterson <59668589+porter-peterson@users.noreply.github.com>
2025-10-12 23:05:24 -0600

    Add basic routing, add skeleton page components, add styling to indicate which page is selected

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
