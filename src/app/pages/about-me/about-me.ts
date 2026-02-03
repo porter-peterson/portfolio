06dbaa28d50b936054cc3db5201150649c748d5c
Porter Peterson <59668589+porter-peterson@users.noreply.github.com>
2026-02-02 19:06:07 -0700

    About Me page, styling and text. Mobile breakpoints for it and header. Improved placeholders

--- src/app/pages/about-me/about-me.ts
+++ src/app/pages/about-me/about-me.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+  imports: [ MatCardModule, MatIconModule, RouterLink, RouterLinkActive, MatButtonModule ],
   templateUrl: './about-me.html',
   styleUrl: './about-me.scss'
 })
