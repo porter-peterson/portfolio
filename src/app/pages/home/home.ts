bbdd627544c4fb12a50e84825770ab14f4faa04f
Porter Peterson <59668589+porter-peterson@users.noreply.github.com>
2026-02-03 15:29:19 -0700

    Contact Page, Added util file for scss that is reused and upadated existing styling to better follow material 3 design

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [MatButtonModule, RouterLink],
+  imports: [ MatButtonModule ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
