c0a061e4569be64544bd12c9c0ca8d7caa945107
Porter Peterson <59668589+porter-peterson@users.noreply.github.com>
2026-02-03 17:03:28 -0700

    Basic projects page, minor UI tweaks to contact page.

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub  } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [
+    FontAwesomeModule,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatIconModule,
+    RouterLink
+  ],
   templateUrl: './projects.html',
   styleUrl: './projects.scss'
 })
 export class Projects {
-
+  faGithub = faGithub;
 }
