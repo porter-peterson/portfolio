b8255540f642cad8c55e65d3b4382679110fbabe
Porter Peterson <59668589+porter-peterson@users.noreply.github.com>
2026-03-19 16:52:43 -0600

    Move to a single page to fix issues on github pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,3 @@
 import { Routes } from '@angular/router';
-import { AboutMe } from './pages/about-me/about-me';
-import { Contact } from './pages/contact/contact';
-import { Projects } from './pages/projects/projects';
 
-export const routes: Routes = [
-  { path: '', redirectTo: 'about-me', pathMatch: 'full' },
-  { path: 'about-me', component: AboutMe},
-  { path: 'contact', component: Contact},
-  { path: 'projects', component: Projects},
-  { path: '**', redirectTo: 'about-me', pathMatch: 'full' } // Wildcard route for a 404 page @TODO: Add 404 page.
-];
+export const routes: Routes = [];
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { ThemeService } from '../../theme.service';
 
 @Component({
   selector: 'app-header',
-  imports: [MatButtonModule, MatIconModule, RouterLink, RouterLinkActive],
+  imports: [MatButtonModule, MatIconModule, FontAwesomeModule],
   standalone: true,
   providers: [ThemeService],
   templateUrl: './header.html',
@@ -14,5 +15,6 @@ import { ThemeService } from '../../theme.service';
 })
 export class Header {
   themeService = inject(ThemeService);
-  
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
 }
